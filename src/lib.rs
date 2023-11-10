55415ee7479bb2af85be415d641687804d35ceb4
Seyediman Seyedarab <imandevel@gmail.com>
2023-11-11 03:24:01 +0330

    Controller+Refactor: Implement Controller struct

--- src/lib.rs
+++ src/lib.rs
@@ -3,22 +3,23 @@
 //! # Examples
 //!
 //! ```
-//! use riftrace::{self, Tracer, TracingStat};
+//! use riftrace::{Tracer, TracingStat, Controller};
+//! let controller = Controller::new();
 //! // Change current tracer from nop to function_graph
-//! riftrace::set_current_tracer(Tracer::FunctionGraph).unwrap();
+//! controller.set_current_tracer(Tracer::FunctionGraph).unwrap();
 //! // Turn tracing on
-//! riftrace::set_tracing_on(TracingStat::On).unwrap();
+//! controller.set_tracing_on(TracingStat::On).unwrap();
 //! // Limit the trace to only "net*"
-//! riftrace::set_ftrace_filter("net*", false).unwrap();
+//! controller.set_ftrace_filter("net*", false).unwrap();
 //! // Print out the output of the trace in a human readable format
-//! println!("{}", riftrace::trace().unwrap());
+//! println!("{}", controller.trace().unwrap());
 //! ```
 
-mod controllers;
+mod controller;
 mod error;
 mod tracer;
 
-pub use controllers::*;
+pub use controller::*;
 pub use tracer::{Tracer, TracingStat};
 pub type RifError = error::Error;
 pub type RifResult<T> = Result<T, RifError>;
